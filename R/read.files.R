7d26aac4e16468966fe3cbad8ffb54dd98bf686a
Ting-Yuan Liu <tliu@fhcrc.org>
2005-11-04 09:14:05 +0000

    Fix the read.files() bug in the Win32 platform. The bug is due to the bug in read.csv/table. More precisely, read.csv/table can not read a gzfile() file correctly. See the following posts for more information: https://stat.ethz.ch/pipermail/r-devel/2004-March/029135.html https://stat.ethz.ch/pipermail/r-devel/2004-March/029144.html Solution:  Using readLines instead of read.csv/table
    git-svn-id: file:///home/git/hedgehog.fhcrc.org/bioconductor/trunk/madman/Rpacks/PROcess@14703 bc3139a8-67e5-0310-9ffc-ced21a209358

--- R/read.files.R
+++ R/read.files.R
@@ -1,6 +1,30 @@
 read.files <- function(fn) {
-	if ("gz" %in% strsplit(fn,split="\\.")[[1]])
-        	fn <- gzfile(fn)
-        read.csv(fn, as.is=TRUE)
+	if ("gz" %in% strsplit(fn,split="\\.")[[1]]){
+            fn <- gzfile(fn)
+        }else{
+            fn <- file(fn)
+        }
+
+        ## read.csv/table has bugs in the win32 platform
+        ## see:
+        ## https://stat.ethz.ch/pipermail/r-devel/2004-March/029135.html
+        ## https://stat.ethz.ch/pipermail/r-devel/2004-March/029144.html
+        ##
+        ##read.csv(fn, as.is=TRUE)
+
+        open(fn, "r")
+        on.exit(close(fn))
+        
+        header <- readLines(fn, n=1)
+        header <- unlist(strsplit(header, ","))
+        
+        tmpRead <- readLines(fn)
+        tmpRead <- strsplit(tmpRead, ",")
+        tmpMatrix <- matrix(NA, nrow=length(tmpRead), ncol=2)
+        for(i in 1:ncol(tmpMatrix)){
+            tmpMatrix[,i] <- sapply(tmpRead, function(x) as.numeric(x[i]))
+        }
+        colnames(tmpMatrix) <- header
+        return(data.frame(tmpMatrix))
 }
 
