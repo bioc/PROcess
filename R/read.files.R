1197c00f2cd952d8326aeb8da6fe96842a47e72f
Ting-Yuan Liu <tliu@fhcrc.org>
2005-11-07 05:58:41 +0000

    Using gzfile(*, "rb") to fix the Win32 error in the monograph.
    git-svn-id: file:///home/git/hedgehog.fhcrc.org/bioconductor/trunk/madman/Rpacks/PROcess@14728 bc3139a8-67e5-0310-9ffc-ced21a209358

--- R/read.files.R
+++ R/read.files.R
@@ -1,19 +1,18 @@
-read.files <- function(fn) {
+ read.files <- function(fn) {
 	if ("gz" %in% strsplit(fn,split="\\.")[[1]]){
             fn <- gzfile(fn)
+            open(fn, "rb") ## using "rb" because gzfile is binary
         }else{
-            fn <- file(fn)
+            fn <- file(fn, "r")
         }
-
+        on.exit(close(fn))
+        
         ## read.csv/table has bugs in the win32 platform
         ## see:
         ## https://stat.ethz.ch/pipermail/r-devel/2004-March/029135.html
         ## https://stat.ethz.ch/pipermail/r-devel/2004-March/029144.html
         ##
         ##read.csv(fn, as.is=TRUE)
-
-        open(fn, "r")
-        on.exit(close(fn))
         
         header <- readLines(fn, n=1)
         header <- unlist(strsplit(header, ","))
