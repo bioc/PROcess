47249f12ca82b761be366a558cb8cfaa8acdd127
Ting-Yuan Liu <tliu@fhcrc.org>
2005-11-07 22:57:46 +0000

    Fix bugs which cause errors in monograph: PROcess/R/read.files.R: use open(gefile(*), open="rb") to fix the bug in Win32 platform
    git-svn-id: file:///home/git/hedgehog.fhcrc.org/bioconductor/branches/RELEASE_1_7/madman/Rpacks/PROcess@14741 bc3139a8-67e5-0310-9ffc-ced21a209358

--- R/read.files.R
+++ R/read.files.R
@@ -1,6 +1,29 @@
-read.files <- function(fn) {
-	if ("gz" %in% strsplit(fn,split="\\.")[[1]])
-        	fn <- gzfile(fn)
-        read.csv(fn, as.is=TRUE)
+ read.files <- function(fn) {
+	if ("gz" %in% strsplit(fn,split="\\.")[[1]]){
+            fn <- gzfile(fn)
+            open(fn, "rb") ## using "rb" because gzfile is binary
+        }else{
+            fn <- file(fn, "r")
+        }
+        on.exit(close(fn))
+        
+        ## read.csv/table has bugs in the win32 platform
+        ## see:
+        ## https://stat.ethz.ch/pipermail/r-devel/2004-March/029135.html
+        ## https://stat.ethz.ch/pipermail/r-devel/2004-March/029144.html
+        ##
+        ##read.csv(fn, as.is=TRUE)
+        
+        header <- readLines(fn, n=1)
+        header <- unlist(strsplit(header, ","))
+        
+        tmpRead <- readLines(fn)
+        tmpRead <- strsplit(tmpRead, ",")
+        tmpMatrix <- matrix(NA, nrow=length(tmpRead), ncol=length(header))
+        for(i in 1:ncol(tmpMatrix)){
+            tmpMatrix[,i] <- sapply(tmpRead, function(x) as.numeric(x[i]))
+        }
+        colnames(tmpMatrix) <- header
+        return(data.frame(tmpMatrix))
 }
 
